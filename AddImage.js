436bbb73411dcbd7ebf344176ab539d4338bf974
DyNamiC <imsong1997@gmail.com>
2020-05-29 23:27:02 +0900

    Update AddImage.js

--- AddImage.js
+++ AddImage.js
@@ -17,6 +17,7 @@ function removeUpload() {
     $('.file-upload-input').replaceWith($('.file-upload-input').clone());
     $('.file-upload-content').hide();
     $('.image-upload-wrap').show();
+    $('.start-image').show();
     $('#label-container').empty();
     $('#contents').empty();
 }
