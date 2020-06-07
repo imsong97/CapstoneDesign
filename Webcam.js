d407d7a8f0585aa902bc186293937308f7be1147
DyNamiC <imsong1997@gmail.com>
2020-06-07 22:49:56 +0900

    Update Webcam.js

--- Webcam.js
+++ Webcam.js
@@ -3,7 +3,7 @@ const URL_w = "./my_model/";
 let webcam, model_w, labelContainer_w, maxPredictions_w;
 
 const btnCamera = document.querySelector(".camera-btn");
-const content = document.getElementById("contents");
+const content_w = document.getElementById("contents");
 
 // Load the image model and setup the webcam
 async function webcaminit() {
@@ -55,11 +55,11 @@ async function predict() {
         labelContainer_w.childNodes[i].innerHTML = classPrediction;
     }
     if (prediction[0].probability>=0.7){
-        content.innerText = "--사람을 만나 당신의 긍정적인 에너지를 나누어 주세요";
+        content_w.innerText = "--사람을 만나 당신의 긍정적인 에너지를 나누어 주세요";
     }else if((prediction[0].probability>=0.4)&&(prediction[0].probability<0.7)){
-        content.innerText = "--감정의 변화가 필요해요, 좋아하는 음악을 찾아 감성을 발휘해 보는 것이 어떨까요?";
+        content_w.innerText = "--감정의 변화가 필요해요, 좋아하는 음악을 찾아 감성을 발휘해 보는 것이 어떨까요?";
     }else{
-        content.innerText = "--운동을 하여 부정적인 감정을 해소시키는 것이 좋겠군요?";
+        content_w.innerText = "--운동을 하여 부정적인 감정을 해소시키는 것이 좋겠군요?";
     }
 }
 
