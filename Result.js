a58f970e7b06e4385d4173795edcc4f52dc7b4d2
DyNamiC <imsong1997@gmail.com>
2020-06-21 21:21:09 +0900

    Update Result.js

--- Result.js
+++ Result.js
@@ -61,16 +61,14 @@ async function predict() {
                                 + "<div class='contents-music'>아티스트 - 노래제목<br>아티스트 - 노래제목</div>" 
                                 + "<div class='contents-youtube'><a href="+data.youtube[Math.floor(Math.random()*data.youtube.length)]+" target='_blank'>" + "기분전환을 위한 유튜브 영상:)" + "</a></div>"
                                 + "<div class='contents-link'><a href="+data.smile[Math.floor(Math.random()*data.smile.length)]+" target='_blank'>" + "웃음의 기능" + "</a></div>"
-                                + "<div class='contents-book-smile'><img src="+data.book_smile[Math.floor(Math.random()*data.book_smile.length)]+" alt='책 소개'/></div>"
-                                + "<div class='contents-book-smile'><img src="+data.book_smile[Math.floor(Math.random()*data.book_smile.length)]+" alt='책 소개'/></div>";
+                                + "<div class='contents-book-smile'><img src="+data.book_smile[Math.floor(Math.random()*data.book_smile.length)]+" alt='책 소개'/><img src="+data.book_smile[Math.floor(Math.random()*data.book_smile.length)]+" alt='책 소개'/></div>";
         }else if(prediction[0].probability>=0.4){
             content.innerHTML = "<div class='res-title'><strong>감정의 변화가 필요해요. 좋아하는 음악을 찾아 감성을 발휘해 보는 것이 어떨까요?</strong></div>"
                                 + "<div class='contents-music'>아티스트 - 노래제목<br>아티스트 - 노래제목</div>" 
                                 + "<div class='contents-youtube'><a href="+data.youtube[Math.floor(Math.random()*data.youtube.length)]+" target='_blank'>" + "기분전환을 위한 유튜브 영상:)" + "</a></div>"
                                 + "<div class='contents-link'><a href="+data.relaxation[Math.floor(Math.random()*data.relaxation.length)]+" target='_blank'>" + "기분전환하는 여러가지 방법!" + "</a></div>"
                                 + "<div class='contents-link'><a href="+data.smile[Math.floor(Math.random()*data.smile.length)]+" target='_blank'>" + "웃음의 기능" + "</a></div>"
-                                + "<div class='contents-book-smile'><img src="+data.book_smile[Math.floor(Math.random()*data.book_smile.length)]+" alt='책 소개'/></div>"
-                                + "<div class='contents-book-angry'><img src="+data.book_angry[Math.floor(Math.random()*data.book_angry.length)]+" alt='책 소개'/></div>"
+                                + "<div class='contents-book-smile'><img src="+data.book_smile[Math.floor(Math.random()*data.book_smile.length)]+" alt='책 소개'/><img src="+data.book_angry[Math.floor(Math.random()*data.book_angry.length)]+" alt='책 소개'/></div>"
                                 + "<div class='contents-comment'>"+data.comment[Math.floor(Math.random()*data.comment.length)]+"</div>";
         }else{
             content.innerHTML = "<div class='res-title'><strong>기분이 안좋아보이네요. 부정적인 감정을 해소시키는 것이 좋겠군요</strong></div>"
@@ -78,8 +76,7 @@ async function predict() {
                                 + "<div class='contents-youtube'><a href="+data.youtube[Math.floor(Math.random()*data.youtube.length)]+" target='_blank'>" + "기분전환을 위한 유튜브 영상:)" + "</a></div>"
                                 + "<div class='contents-link'><a href="+data.relaxation[Math.floor(Math.random()*data.relaxation.length)]+" target='_blank'>" + "기분전환하는 여러가지 방법!" + "</a></div>"
                                 + "<div class='contents-link'><a href="+data.smile[Math.floor(Math.random()*data.smile.length)]+" target='_blank'>" + "웃음의 기능" + "</a></div>"
-                                + "<div class='contents-book-angry'><img src="+data.book_angry[Math.floor(Math.random()*data.book_angry.length)]+" alt='책 소개'/></div>"
-                                + "<div class='contents-book-angry'><img src="+data.book_angry[Math.floor(Math.random()*data.book_angry.length)]+" alt='책 소개'/></div>"
+                                + "<div class='contents-book-angry'><img src="+data.book_angry[Math.floor(Math.random()*data.book_angry.length)]+" alt='책 소개'/><img src="+data.book_angry[Math.floor(Math.random()*data.book_angry.length)]+" alt='책 소개'/></div>"
                                 + "<div class='contents-comment'>"+data.comment[Math.floor(Math.random()*data.comment.length)]+"</div>";
         }
     });
